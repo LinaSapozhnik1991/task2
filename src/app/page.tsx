29071bbcdcdda0320dacc4367aa98227b64d79ad
Sapozhnik <yasapozhnik@gmail.com>
2025-02-22 23:05:00 +0900

    delete comments, formated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
-'use client'
-import { useState } from 'react';
-import styles from '../scss/page.module.scss'
-import AddFileModal from '@/widgets/modals/files/addFile';
+"use client";
 
+import styles from "../scss/page.module.scss";
 
 export default function Home() {
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-      setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-      setIsModalOpen(false);
-
-  }
-
- 
-
-  return (
-    <div className={styles.page}>
-     
-     <button onClick={handleOpenModal}>Добавить файл</button>
-     <AddFileModal isOpen={isModalOpen} onClose={handleCloseModal} />
-   
-    </div>
-  );
-};
-
+  return <div className={styles.page}></div>;
+}
--- src/widgets/modals/files/addFile/index.tsx
+++ src/widgets/modals/files/addFile/index.tsx
@@ -1,85 +1,97 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import styles from './AddFiles.module.scss';
-import FileUploader from '../../../../shared/ui/FileUploader/FileUploader'; 
-import FileUpload from '../../../../shared/ui/FileUpload/FileUpload'; 
-import { Button } from '../../../../shared/ui/Button/Button'; 
-import { ButtonColors } from '../../../../shared/ui/Button/Button.types';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "./AddFiles.module.scss";
+import FileUploader from "../../../../shared/ui/FileUploader/FileUploader";
+import FileUpload from "../../../../shared/ui/FileUpload/FileUpload";
+import { Button } from "../../../../shared/ui/Button/Button";
+import { ButtonColors } from "../../../../shared/ui/Button/Button.types";
 
 interface AddFileModalProps {
-    isOpen: boolean;
-    onClose: () => void;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 const AddFileModal: React.FC<AddFileModalProps> = ({ isOpen, onClose }) => {
-    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-    const [isUploading, setIsUploading] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
 
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    const handleFileChange = (files: File[]) => {
-        setSelectedFiles(files); 
-    };
+  const handleFileChange = (files: File[]) => {
+    setSelectedFiles(files);
+  };
 
-    const handleAddFile = () => {
-        if (selectedFiles.length === 0) return;
+  const handleAddFile = () => {
+    if (selectedFiles.length === 0) return;
 
-        setIsUploading(true); 
+    setIsUploading(true);
 
-        const totalFiles = selectedFiles.length;
-        selectedFiles.forEach((file, index) => {
-            const uploadFile = () => {
-                let progress = 0;
+    const totalFiles = selectedFiles.length;
+    selectedFiles.forEach((file, index) => {
+      const uploadFile = () => {
+        let progress = 0;
 
-                const interval = setInterval(() => {
-                    if (progress < 100) {
-                        progress += 10; 
-                    } else {
-                        clearInterval(interval);
-                        if (index === totalFiles - 1) {
-                            setIsUploading(false); 
-                            onClose(); 
-                        }
-                    }
-                }, 300); 
-            };
+        const interval = setInterval(() => {
+          if (progress < 100) {
+            progress += 10;
+          } else {
+            clearInterval(interval);
+            if (index === totalFiles - 1) {
+              setIsUploading(false);
+              onClose();
+            }
+          }
+        }, 300);
+      };
 
-            uploadFile();
-        });
-    };
+      uploadFile();
+    });
+  };
 
-    return ReactDOM.createPortal(
-        <div className={styles.overlayStyle}>
-            <div className={styles.modalStyle}>
-                <h2>Добавить файлы</h2>
-                <div className={styles.modalContent}>
-                {isUploading ? (
-                    selectedFiles.map((file, index) => ( // Добавлено 'index' как второй аргумент
-                        <FileUpload
-                            key={file.name}
-                            onFileChange={() => {}} 
-                            isUploading={isUploading}
-                            uploadProgress={(index + 1) * 10} // Передаем прогресс загрузки
-                        />
-                    ))
-                ) : (
-                    <FileUploader onFileChange={handleFileChange} />
-                )}
-                </div>
-                <div className={styles.buttonContainerStyle}>
-                    <Button 
-                      style={{ width: "264px" }} backgroundColor={ButtonColors.Primary} onClick={handleAddFile} disabled={isUploading}>
-                        Добавить
-                    </Button>
-                    <Button
-                    style={{ width: "264px" }} backgroundColor={ButtonColors.Secondary} onClick={onClose}>
-                        Отмена
-                    </Button>
-                </div>
-            </div>
-        </div>,
-        document.body 
-    );
+  return ReactDOM.createPortal(
+    <div className={styles.overlayStyle}>
+      <div className={styles.modalStyle}>
+        <h2>Добавить файлы</h2>
+        <div className={styles.modalContent}>
+          {isUploading ? (
+            selectedFiles.map(
+              (
+                file,
+                index 
+              ) => (
+                <FileUpload
+                  key={file.name}
+                  onFileChange={() => {}}
+                  isUploading={isUploading}
+                  uploadProgress={(index + 1) * 10} 
+                />
+              )
+            )
+          ) : (
+            <FileUploader onFileChange={handleFileChange} />
+          )}
+        </div>
+        <div className={styles.buttonContainerStyle}>
+          <Button
+            style={{ width: "264px" }}
+            backgroundColor={ButtonColors.Primary}
+            onClick={handleAddFile}
+            disabled={isUploading}
+          >
+            Добавить
+          </Button>
+          <Button
+            style={{ width: "264px" }}
+            backgroundColor={ButtonColors.Secondary}
+            onClick={onClose}
+          >
+            Отмена
+          </Button>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
 };
 
 export default AddFileModal;
